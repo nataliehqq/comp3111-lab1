af07fd81d78b4ac559476a94523e4083dbb255f1
nat <nat@DESKTOP-C20G08U>
2022-09-17 13:51:19 +0800

    I have make a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,6 +7,8 @@ public class Library {
 	
 	/* Add this function */
 	public static void main(String arg[]) {
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
 			System.out.println("Hello, World!");
 	}
 	
